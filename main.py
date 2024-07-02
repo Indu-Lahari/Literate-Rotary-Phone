b53c81893fdd74901b366e28031dfab39eece50f
Indu <indulahari6@gmail.com>
2024-07-02 14:40:20 +0530

    Create file uploader component allowing user to upload file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import streamlit
 from PIL import Image
 
+streamlit.subheader("Color to Grayscale Converter")
+
+# Create a file uploader component allowing the user to upload a file
+uploaded_image = streamlit.file_uploader("Upload Image")
+
 #Start the camera
 with streamlit.expander("Start Camera"):
     camera_image = streamlit.camera_input("Camera")
@@ -14,3 +19,12 @@ if camera_image:
 
     #Render the grayscale image to the webpage
     streamlit.image(gray_image)
+
+# Check if the image exists meaning the user has uploaded a file
+if uploaded_image:
+    # Open the user uploaded image with PIL
+    img = Image.open(uploaded_image)
+    # Convert the image to grayscale
+    gray_uploaded_img = img.convert('L')
+    # Display the grayscale image on the webpage
+    streamlit.image(gray_uploaded_img)
